04c1f519da548d74516539478015d5b668b2000c
Lucy Wall <lhwall@gmail.com>
2018-03-06 15:28:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-odd_lines = "   |   |   "
-even_lines = "-----------"
+
 
 def display_board
+  odd_lines = "   |   |   "
+  even_lines = "-----------"
+  
   put odd_lines
   put even_lines
   put odd_lines
